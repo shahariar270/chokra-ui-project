c0a80aab798d18fb2c39ff75b2f4168186234e47
shahariar270 <mdshahariar102@gmail.com>
2025-05-20 21:40:27 +0600

    improve: markup

--- src/includes/TodoFrom/index.jsx
+++ src/includes/TodoFrom/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Center, Container, Field, Flex, Input } from "@chakra-ui/react"
+import { Box, Button, Center, Container, Field, Flex, Input } from "@chakra-ui/react"
 import { TodoList } from '../TodoList'
 
 export const TodoFrom = () => {
@@ -20,27 +20,39 @@ export const TodoFrom = () => {
     }
   return (
       <>
-      <Container display='flex'>
-        <Flex>
-          <Field.Root>
+<Box>
+ <Flex p={3} gap={3}>
+       <Field.Root display='flex' width='500px' flexDirection='row' gap='16px'>
             <Input
               value={todo}
               onChange={(e)=> setTodo(e.target.value)}
-              border='black'   
+              border='black'
+              width='350px'
+              placeholder='Add Todo Value'
+              borderColor='black'
+              _placeholder={{ opacity: 1, color: 'gray.500' }}
            />
            <Button
               onClick={valueSubmit}
-              color='black'
+              color='white'
               bg='teal'
+              size='md'
+              outline='none'
            >Add Value</Button>
           </Field.Root>
         </Flex>
-      </Container>
+
+</Box>
+     
+
+      {/* <Container display='flex'>
+        <Flex>
+         
+      </Container> */}
       <TodoList
         todos={todos}
         setTodos={setTodos} 
       />
       </>
-
   )
 } 
