57619e3e5c5207932536d2091879ffb9022a5c8d
shahariar270 <mdshahariar102@gmail.com>
2025-05-21 06:54:02 +0600

    implement: dnd

--- src/includes/TodoList/index.jsx
+++ src/includes/TodoList/index.jsx
@@ -1,38 +1,84 @@
-import { Box, Button, Checkbox, List } from '@chakra-ui/react'
+import React from 'react';
+import {
+  DndContext,
+  closestCenter,
+  useSensor,
+  useSensors,
+  PointerSensor
+} from '@dnd-kit/core';
+
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import { CSS } from '@dnd-kit/utilities';
+import { Box, Button, List, ListItem } from '@chakra-ui/react';
 import { MdDelete } from "react-icons/md";
 
+export const TodoList = ({ todos, setTodos }) => {
+  const sensors = useSensors(useSensor(PointerSensor));
 
-export const TodoList = ({todos,setTodos}) => {
-const removeTodo = (id) =>{
-      setTodos(todos.filter(item => item.id !== id));
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id !== over?.id) {
+      const oldIndex = todos.findIndex(todo => todo.id === active.id);
+      const newIndex = todos.findIndex(todo => todo.id === over.id);
+      setTodos(arrayMove(todos, oldIndex, newIndex));
+    }
+  };
 
-}
   return (
-    <div>
-       <List.Root>
-        {
-          todos.map(item =>(
-            <div>
-              <Box bg='tomato' w='100%' p={1} color='white' margin={2}>
-            <List.Item key={item.id} display='flex' width='100%' justifyContent='space-between'>
-              {item.value}
-              <Button 
-                onClick={()=> removeTodo(item.id) }  
-                color='black'
-                colorScheme='gray'
-                border='none'
-                outline='none'
-              >
-        <MdDelete />
-      </Button>
-            </List.Item>
-            </Box>
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={todos.map(todo => todo.id)} strategy={verticalListSortingStrategy}>
+        <List.Root spacing={3}>
+          {todos.map(item => (
+            <SortableItem key={item.id} item={item} removeTodo={() => setTodos(todos.filter(todo => todo.id !== item.id))} />
+          ))}
+        </List.Root>
+      </SortableContext>
+    </DndContext>
+  );
+};
 
-            </div>  
-          ))
-        }
-       </List.Root>
-    </div>
-    
-  )
-}
+const SortableItem = ({ item, removeTodo }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: item.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <List.Item ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <Box
+        bg='tomato'
+        w='100%'
+        p={3}
+        color='white'
+        display='flex'
+        alignItems='center'
+        justifyContent='space-between'
+        borderRadius='md'
+        boxShadow='md'
+      >
+        {item.value}
+        <Button
+          onClick={removeTodo}
+          colorScheme='gray'
+          size='sm'
+        >
+          <MdDelete />
+        </Button>
+      </Box>
+    </List.Item>
+  );
+};
