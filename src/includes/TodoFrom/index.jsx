29098eb93f43be9b7123fc3b0815150390eebf0c
shahariar270 <mdshahariar102@gmail.com>
2025-05-24 10:46:31 +0600

    remove: edit logic

--- src/includes/TodoFrom/index.jsx
+++ src/includes/TodoFrom/index.jsx
@@ -1,57 +1,63 @@
 import React, { useState } from 'react'
-import { Box, Button, Flex, Input } from "@chakra-ui/react"
+import { Box, Button, Center, Container, Field, Flex, Input } from "@chakra-ui/react"
 import { TodoList } from '../TodoList'
 import { useDispatch, useSelector } from 'react-redux'
-import { addTodo, updateTodo } from '../../reduer/TodoSlice/TodoSlice'
+import { addTodo, editMode, updateTodo } from '../../reduer/TodoSlice/TodoSlice'
 
 export const TodoFrom = () => {
   const [todo, setTodo] = useState('');
   const [editTodo, setEditTodo] = useState('');
   const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todo.todo);
   const isEdit = useSelector((state) => state.todo.isEdit);
-  const editId = useSelector((state) => state.todo.editId); // if available
+  const [editId, setEditId] = useState(null)
 
+  const editHandle = (id) => {
+    dispatch(editMode(!isEdit))
+    setEditId(id)
+  }
   const valueSubmit = (e) => {
     e.preventDefault();
     if (todo.trim() !== '') {
-      dispatch(addTodo(todo));
+
+      if (isEdit) {
+        dispatch(updateTodo(editId, editTodo))
+        // setTodo(editTodo)
+      } else {
+        dispatch(addTodo(todo));
+      }
       setTodo('');
     }
   };
 
-  const editHandle = (e) => {
-    e.preventDefault();
-    if (editTodo.trim() !== '') {
-      dispatch(updateTodo({ id: editId, value: editTodo }));
-      setEditTodo('');
-    }
-  };
-
   return (
-    <Box>
-      <Flex p={3} gap={3}>
-        <Flex width='500px' flexDirection='row' gap='16px'>
-          <Input
-            // value={isEdit ? editTodo : todo}
-            onChange={(e) => isEdit ? setEditTodo(e.target.value) : setTodo(e.target.value)}
-            border='1px'
-            borderColor='black'
-            width='350px'
-            placeholder='Add Todo Value'
-            _placeholder={{ opacity: 1, color: 'gray.500' }}
-          />
-          <Button
-            onClick={(e) => isEdit ? editHandle(e) : valueSubmit(e)}
-            color='white'
-            bg='teal'
-            size='md'
-            outline='none'
-          >
-            {isEdit ? 'Update' : 'Add'}
-          </Button>
+    <>
+      <Box>
+        <Flex p={3} gap={3}>
+          <Field.Root display='flex' width='500px' flexDirection='row' gap='16px'>
+            <Input
+              value={isEdit ? editTodo : todo}
+              onChange={(e) => isEdit ? setEditTodo(e.target.value) : setTodo(e.target.value)}
+              border='black'
+              width='350px'
+              placeholder='Add Todo Value'
+              borderColor='black'
+              _placeholder={{ opacity: 1, color: 'gray.500' }}
+            />
+            <Button
+              onClick={(e) => valueSubmit(e)}
+              color='white'
+              bg='teal'
+              size='md'
+              outline='none'
+            >Add Value</Button>
+          </Field.Root>
         </Flex>
-      </Flex>
-      <TodoList />
-    </Box>
-  );
+      </Box>
+      <TodoList
+        editHandle={editHandle}
+      />
+    </>
+  )
 }
+
--- src/includes/TodoList/index.jsx
+++ src/includes/TodoList/index.jsx
@@ -1,61 +1,61 @@
 import { Box, Button, Checkbox, List } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { MdDelete } from "react-icons/md";
-import {  useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { editMode, removeTodo } from '../../reduer/TodoSlice/TodoSlice';
 
 
-export const TodoList = () => {
+export const TodoList = ({ editHandle }) => {
   // const {isEdit, setIsEdit} =useState(false)
 
-  const dispatch= useDispatch()
+  const dispatch = useDispatch()
 
-  const todos = useSelector((state)=> state.todo.todo)
-  const isEdit = useSelector((state)=> state.todo.isEdit)
-  
-  const editHandle =(id)=>{
-    dispatch(editMode(!isEdit))
+  const todos = useSelector((state) => state.todo.todo)
+  const isEdit = useSelector((state) => state.todo.isEdit)
+
+  // const editHandle =(id)=>{
+  //   dispatch(editMode(!isEdit))
+  // }
+
+  const removeHandle = (id) => {
+    dispatch(removeTodo(id));
   }
-
-const removeHandle = (id) =>{
-     dispatch(removeTodo(id));
-}
 
   return (
     <div>
-       <List.Root>
+      <List.Root>
         {
-          todos.map(item =>(
+          todos.map(item => (
             <div>
               <Box bg='tomato' w='100%' p={1} color='white' margin={2}>
-            <List.Item key={item.value} display='flex' width='100%' justifyContent='space-between'>
-              {item.value}
-              <Button 
-                onClick={()=> removeHandle(item.id) }  
-                color='black'
-                colorScheme='gray'
-                border='none'
-                outline='none'
-              >
-        <MdDelete />
-      </Button>
-              <Button 
+                <List.Item key={item.id} display='flex' width='100%' justifyContent='space-between'>
+                  {item.value}
+                  <Button
+                    onClick={() => removeHandle(item.id)}
+                    color='black'
+                    colorScheme='gray'
+                    border='none'
+                    outline='none'
+                  >
+                    <MdDelete />
+                  </Button>
+                  {/* <Button 
                 onClick={()=> editHandle(item.id) }  
                 color='white'
                 colorScheme='blue'
                bg='blue'
               >
         <MdDelete />
-      </Button>
-            </List.Item>
-            </Box>
+      </Button> */}
+                </List.Item>
+              </Box>
 
             </div>
-             
+
           ))
         }
-       </List.Root>
-      
+      </List.Root>
+
     </div>
   )
 }
