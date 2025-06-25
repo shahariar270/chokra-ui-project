800322f43d412231dd7fad3daa044bdc4c7cbf3e
shahariar270 <mdshahariar102@gmail.com>
2025-06-25 21:32:01 +0600

    load data

--- src/includes/DynamicRoute/indev.jsx
+++ src/includes/DynamicRoute/indev.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export const DynamicRoute = () => {
-    
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/posts')
+      .then(res => res.json())
+      .then(data=> setData(data.posts));
+  }, [])
+
   return (
-    <div>DynamicRoute</div>
+    <div>
+      <h1>Dynamic Route</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
