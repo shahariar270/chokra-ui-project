f60d7ab5dd586066cff3e7d64394c9025261bafa
shahariar270 <mdshahariar102@gmail.com>
2025-06-26 21:40:50 +0600

    implement card on chaakra

--- src/includes/DynamicRoute/indev.jsx
+++ src/includes/DynamicRoute/indev.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useEffect } from 'react'
+import './styles.css'
+import { Button, Card, Text } from '@chakra-ui/react';
+
 
 export const DynamicRoute = () => {
   const [data, setData] = React.useState([]);
@@ -7,20 +10,29 @@ export const DynamicRoute = () => {
   useEffect(() => {
     fetch('https://dummyjson.com/posts')
       .then(res => res.json())
-      .then(data=> setData(data.posts));
+      .then(data => setData(data.posts));
   }, [])
 
   return (
-    <div>
-      <h1>Dynamic Route</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.body}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="dynamic-route">
+
+      {
+        data.map((item) => (
+          <div key={item.id} className="card">
+            <Card.Root>
+              <Card.Header>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Description>{item.body}</Card.Description>
+              </Card.Header>
+              <Button
+                color='white'
+                background='black'
+                _hover={{ background: 'gray.700' }}
+              >Learn more</Button>
+            </Card.Root>
+          </div>
+        ))
+      }
     </div>
   )
 }
