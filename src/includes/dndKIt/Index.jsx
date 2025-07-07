9876af235bcacf7e47c5b65e37232fdf3c84672e
shahariar270 <mdshahariar102@gmail.com>
2025-07-07 07:29:15 +0600

    implement table

--- src/includes/dndKIt/Index.jsx
+++ src/includes/dndKIt/Index.jsx
@@ -1,10 +1,29 @@
+import { Table } from '@chakra-ui/react'
 import React from 'react'
 
 export const DndKit
- = () => {
-  return (
-    <div>Dndkit
+    = () => {
+        return (
+            <>
+                <Table.Root>
+                    <Table.ColumnGroup>
+                        <Table.Column htmlWidth="50%" />
+                        <Table.Column htmlWidth="40%" />
+                        <Table.Column />
+                    </Table.ColumnGroup>
+                    <Table.Row>
+                        <Table.ColumnHeader>Product</Table.ColumnHeader>
+                        <Table.ColumnHeader>Category</Table.ColumnHeader>
+                        <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
+                    </Table.Row>
 
-    </div>
-  )
-}
+                    <Table.Row>
+                        <Table.Cell>heelo</Table.Cell>
+                        <Table.Cell>Category</Table.Cell>
+                        <Table.Cell textAlign="end">120</Table.Cell>
+                    </Table.Row>
+                </Table.Root>
+            </>
+        )
+
+    }
