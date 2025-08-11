308531ef8ad7701ffb983d47caa911a278746db7
shahariar270 <mdshahariar102@gmail.com>
2025-08-11 23:02:22 +0600

    implement style

--- src/dev/Crud/index.jsx
+++ src/dev/Crud/index.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css'
 
 export const Crud = () => {
-  return (
-    <div>Crud</div>
-  )
+    const [text, setText] = useState('');
+    const [items, setItems] = useState([]);
+
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        setItems([...items, text])
+
+
+    }
+
+    return (
+        <>
+            <form action="" onSubmit={submitHandler}>
+                <label htmlFor="text">
+                    added text
+                    <input type="text" name='text' onChange={(e) => setText(e.target.value)} />
+                    <button type="submit">added data</button>
+                </label>
+            </form>
+            <ul>
+                {
+                    items.map((item, index) => (
+
+                        // console.log(item)
+                        <li
+                            key={index}
+                            className="list-item"
+                        >
+                            <span>{item}</span>
+                            <button 
+                            className="list-button edit-button"
+                            >edit</button>
+                            <button className="list-button delete-button">delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
 }
