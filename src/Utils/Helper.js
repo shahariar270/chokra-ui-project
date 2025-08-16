4b30a76779a5842d8c3896a88a7ec63d117ecdec
shahariar270 <mdshahariar102@gmail.com>
2025-08-16 10:43:30 +0600

    update rendom data

--- src/Utils/Helper.js
+++ src/Utils/Helper.js
@@ -12,7 +12,7 @@ export const blogData = async () => {
 };
 
 export const RandomId = (length = 8) => {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
     let result='';
     for (let i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
