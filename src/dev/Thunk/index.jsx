554cb1bb7cab55a7a2afd25a56abb47a2bc82b42
shahariar270 <mdshahariar102@gmail.com>
2025-08-19 22:11:42 +0600

    implement post data

--- src/dev/Thunk/index.jsx
+++ src/dev/Thunk/index.jsx
@@ -1,6 +1,7 @@
 import React, { use, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addUser, fetchUsers } from '@reduer/thunk/index'
+import { Formik } from 'formik';
 
 export const Thunk = () => {
   const dispatch = useDispatch();
@@ -15,11 +16,41 @@ export const Thunk = () => {
   const handleAddUser = () => {
     dispatch(addUser({ name: "shahariar" }));
   };
+
   return (
-    <div>
-      <button onClick={handleAddUser}>
-        Add User
-      </button>
-    </div>
+    <React.Fragment>
+      <Formik
+        initialValues={{
+          name: '',
+          username: '',
+          email: ''
+        }}
+        onSubmit={(values) => {
+          dispatch(addUser(values))
+        }}
+      >
+        {
+          ({
+            values,
+            // onSubmit,
+            handleChange,
+            handleSubmit
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="">Name
+                <input type="text" name='name' value={values.name}  onChange={handleChange} />
+              </label>
+              <label htmlFor="">Name
+                <input type="email" name='email' onChange={handleChange} value={values.email} />
+              </label>
+              <label htmlFor="">Name
+                <input type="text" name='username' value={values.username}  onChange={handleChange} />
+              </label>
+              <button type='submit' >add</button>
+            </form>
+          )
+        }
+      </Formik>
+    </React.Fragment>
   )
 }
