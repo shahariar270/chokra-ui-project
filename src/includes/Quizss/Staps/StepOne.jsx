64678289a86a5c6612805cb43684230be02d0f77
shahariar270 <mdshahariar102@gmail.com>
2025-09-26 08:14:31 +0600

    basic quiz context apis

--- src/includes/Quizss/Staps/StepOne.jsx
+++ src/includes/Quizss/Staps/StepOne.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuizContext } from '../Context'
 
 export const StepOne = () => {
+  let quiz = useContext(QuizContext);
+
+  console.log(quiz);
   return (
     <div>StepOne</div>
   )
--- src/includes/Quizss/index.jsx
+++ src/includes/Quizss/index.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import { StepControl } from './StepControl';
 import './styles.css'
+import { QuizContext } from './Context';
 
 export const Quiz = () => {
   const [openModal, setOpenModal] = useState(false);
+
+let  quizData = [
+    {
+      title: "what is javascript",
+      answer: ['PL', 'markup', 'all'],
+      carectAnswer: 'PL'
+    }
+  ]
 
   return (
     <React.Fragment>
       <button
         onClick={() => setOpenModal(true)}
       >add new</button>
+      <QuizContext.Provider value={quizData}>
+        {openModal && <StepControl />
 
-      { openModal && <StepControl/>
-
-      }
+        }
+      </QuizContext.Provider>
     </React.Fragment>
   )
 }
